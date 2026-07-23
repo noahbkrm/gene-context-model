fixing masking architecture

src/model/dataclass.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 from rna_encoder import RnaStats, RnaEmbedding
 from cnv_encoder import CNVEmbedding
 from snv_encoder import SNVEmbedding
+from constants import BATCH
 
 class TCGA_Dataset(torch.utils.data.Dataset):
 
@@ -37,10 +38,10 @@ class TCGA_Dataset(torch.utils.data.Dataset):
             "cnv_mask": self.cnv_mask[idx],
         }
 
-def get_loader(dataset, shuffle: bool = True):
+def get_loader(dataset, shuffle: bool = True, batch_size: int = BATCH):
     loader = DataLoader(
         dataset,
-        batch_size=32,
+        batch_size=batch_size,
         shuffle=shuffle,
         pin_memory=True
     )

src/model/gene_model.py
@@ -7,21 +7,18 @@ from rna_encoder import RnaEmbedding
 from fusion import GeneTokenEmbedding
 from transformer import TransformerEncoder
 from projection import Projection
-from mask import GeneMask
 
-class GeneModel(nn.Module):
+class GeneTokenizer(nn.Module):
     def __init__(self, n_genes: int, hidden_dim: int = HIDDEN_DIM):
         super().__init__()
-        self.cnv_encoder =  CNVEmbedding(hidden_dim)
-        self.snv_encoder = SNVEmbedding(hidden_dim)
+
         self.rna_encoder = RnaEmbedding(hidden_dim)
-        self.combine_tokens = GeneTokenEmbedding(n_genes, hidden_dim)
-        self.projection = Projection(hidden_dim)
-        self.mask = GeneMask(hidden_dim)
+        self.snv_encoder = SNVEmbedding(hidden_dim)
+        self.cnv_encoder = CNVEmbedding(hidden_dim)
 
-        self.transformer = TransformerEncoder()
+        self.combine_tokens = GeneTokenEmbedding(n_genes, hidden_dim,)
 
-    def forward(self, batch): # image_binary: True is student, False is teacher
+    def forward(self, batch):
 
         rna_tokens = self.rna_encoder(
             batch["rna_expression"],
@@ -30,24 +27,31 @@ class GeneModel(nn.Module):
 
         snv_tokens = self.snv_encoder(
             batch["snv_states"],
-            batch["snv_mask"]
+            batch["snv_mask"],
         )
 
         cnv_tokens = self.cnv_encoder(
             batch["cnv_states"],
-            batch["cnv_mask"]
+            batch["cnv_mask"],
         )
 
-        token_emb = self.combine_tokens(rna_tokens, snv_tokens, cnv_tokens)
+        gene_tokens = self.combine_tokens(rna_tokens, snv_tokens, cnv_tokens,)
 
-        masked_emb, gene_mask = self.mask(token_emb)
+        return gene_tokens
 
-        transformed_emb = self.transformer(masked_emb)
+class GeneModel(nn.Module):
+    def __init__(self, hidden_dim: int = HIDDEN_DIM):
+        super().__init__()
+        self.projection = Projection(hidden_dim)
+        self.transformer = TransformerEncoder()
+
+    def forward(self, gene_tokens): # image_binary: True is student, False is teacher
+
+        transformed_emb = self.transformer(gene_tokens)
 
         proj_emb = self.projection(transformed_emb)
 
         return {
             "projection": proj_emb,
             "embedding": transformed_emb,
-            "mask": gene_mask
         }